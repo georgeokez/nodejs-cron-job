9c5a6cc921453b375914ca42474e5c13469ef15a
georgeokez <georgeokez@gmail.com>
2025-02-10 23:43:22 +0000

    feat: add secret variables to app

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 console.log("Node.js script running at: ", new Date().toLocaleString());
+console.log("Access to secrets via env ---");
+console.log("USERNAME: ", process.env.USERNAME);
+console.log("PASSWORD: ", process.env.PASSWORD);
